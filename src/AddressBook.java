b3f69f9640623e480c7661aea801fbbe1f695e62
Seydi Cheikh wade <seydicheikhwade@gmail.com>
2025-09-24 07:30:17 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,6 +19,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("Test");
     }
 }
 
